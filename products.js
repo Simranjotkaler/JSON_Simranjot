57c602d3e3108aef5e7ce23c7a0a4dd79aa2445d
Simranjotkaler <63378255+Simranjotkaler@users.noreply.github.com>
2020-07-12 17:23:10 -0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,52 @@
+//getting the json file url from the github server
+let url = 'https://github.com/Simranjotkaler/JSON-WEEK8/blob/master/lab-8-files/lab-8-files/products.json';
+
+//using the same function used in lab 4 with url as parameter
+function useAsynchronous(url) {
+    //uisng fetch api insta=ead of xhr object
+    fetch(url).then(function(response) {
+        //fetch api uses promises as you can see we are using then blocks
+        //also here we specify response type like this where json() is kind of a method
+        return response.json();
+    }).then(function(jso) {
+        //passing the json info to jsonObj which is used in next function
+        let jsonObj = jso;
+        //calling the weirdDeals functions
+        weirdDeals(jsonObj);
+        //if there is any error in the steps mentioned above, the catch block will execute
+    }).catch(function(error) {
+        //using alert box here to execute
+        alert('The problem in fetching is: ' + error.message);
+    });
+
+};
+
+//function which actually contains elements to be added dynamically
+function weirdDeals(jsonObj) {
+    let weirdDeals = jsonObj.weirdDeals;
+    //calling the section element
+    let section = document.querySelector('section');
+    for (let i = 0; i < weirdDeals.length; i++) {
+        //creating elements dynamically
+        let article = document.createElement('article');
+        let h2 = document.createElement('h2');
+        let img = document.createElement('img');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        //setting the  attributes
+        img.setAttribute('src', 'https://github.com/Simranjotkaler/JSON-WEEK8/tree/master/lab-8-files/lab-8-files/images' + weirdDeals[i].image);
+        img.setAttribute('alt', weirdDeals[i].image);
+        h2.textContent = weirdDeals[i].name;
+        p1.textContent = 'Price ' + weirdDeals[i].price;
+        p2.textContent = 'Details ' + weirdDeals[i].details;
+        //appending the elements in the section which is in body of html document
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        section.appendChild(article);
+    }
+}
+
+//invoking the function
+useAsynchronous('https://github.com/Simranjotkaler/JSON-WEEK8/blob/master/lab-8-files/lab-8-files/products.json');
